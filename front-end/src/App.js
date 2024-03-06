101ac1a87bdadb45dd2aec2c19d631d24d375afb
JVLENNY10 <joao.lenny100@gmail.com>
2024-03-06 20:46:08 -0300

    Removendo itens desnecessários

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,35 +1,13 @@
-import './App.css';
-
-import { useEffect, useState } from 'react';
-import { getFilms } from './services/get';
 import Feed from './components/Feed';
+import { getFilms } from './services/get';
+import { useEffect, useState } from 'react';
 
 function App() {
-  
-
   return (
     <div className="App">
       <header />
 
       <main>
-        {/* <div className='films-list'>
-          {
-            films.map((film, index) => (
-              <div className='card-film' key={ index }>
-                { console.log(film) }
-                <img alt={ `image_${index}` } src={ process.env.REACT_APP_IMG + film.backdrop_path } />
-
-                <div className='vote-average'>
-                  <FaStar className='star' />
-                  <span>{ parseFloat(film.vote_average).toFixed(1).replace('.', ',') }</span>
-                </div>
-
-                <h1>{ film.title }</h1>
-              </div>
-            ))
-          }
-        </div> */}
-
         <Feed />
       </main>
     </div>
